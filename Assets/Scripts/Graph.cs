9fb5b83cb43497231cd9454788548bd96ff21c73
Michał Machowski <machowski.michal01@gmail.com>
2025-01-07 22:17:01 +0100

    Graph

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,13 +1,49 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class Graph
 {
-    public Node Root;
+    public Dictionary<Vector2, Node> nodes = new Dictionary<Vector2, Node>();
+    public List<(Node, Node)> edges = new List<(Node, Node)>();
+    public float agentWidth = 0.5f;
+
+    public void AddNode(Vector2 position)
+    {
+        if (!nodes.ContainsKey(position))
+        {
+            nodes[position] = new Node(position);
+        }
+    }
+    public void AddEdge(Vector2 pos1, Vector2 pos2)
+    {
+        edges.Add((nodes[pos1], nodes[pos2]));
+    }
+
+    public bool canReach(Vector2 from, Vector2 to)
+    {
+        Vector2 direction = (to - from).normalized;
+        float distance = Vector2.Distance(from, to);
+        var hit = Physics2D.BoxCast(from, new Vector2(agentWidth, agentWidth), 0, direction, distance);
+        return hit.collider == null;
+    }
+
+    public Color nodeColor = Color.green;
+    public Color edgeColor = Color.blue;
+    public float nodeSize = 0.1f;
 }
 
 public class Node
 {
-    public Vector2 Position;
-    public List<Node> Neighbours;
+    public Vector2 Position { get; }
+    public float G { get; set; } = float.MaxValue; // Cost from start node
+    public float H { get; set; } = 0;             // Heuristic cost to goal
+    public float F => G + H;                      // Total cost
+    public bool Visited { get; set; } = false;
+
+    public Node(Vector2 position)
+    {
+        Position = position;
+    }
 }
---
+++ Assets/Scripts/GraphBuilder.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GraphBuilder : MonoBehaviour
+{
+    public Graph graph;
+    private float dist = 1.5f;
+    private Vector2[] directions;
+
+    public void Initialize(Graph graph)
+    {
+        this.graph = graph;
+    }
+
+    void Awake()
+    {
+        directions = new Vector2[]
+        {
+            new Vector2(dist, 0), new Vector2(dist, dist), new Vector2(0, dist), new Vector2(-dist, dist),
+            new Vector2(-dist, 0), new Vector2(-dist, -dist), new Vector2(0, -dist), new Vector2(dist, -dist)
+        };
+    }
+
+    public void fillGraph(Vector2 position)
+    {
+        Queue<Vector2> toVisit = new Queue<Vector2>();
+        toVisit.Enqueue(position);
+        graph.AddNode(position);
+
+        while (toVisit.Count > 0)
+        {
+            var current = toVisit.Dequeue();
+
+            foreach (var dir in directions)
+            {
+                var neighbor = current + dir;
+
+                if (graph.canReach(current, neighbor))
+                {
+                    if (!graph.nodes.ContainsKey(neighbor)) 
+                    {
+                        graph.AddNode(neighbor);
+                        toVisit.Enqueue(neighbor);
+                    }
+                    graph.AddEdge(current, neighbor);
+                }
+            }
+        }
+        Debug.Log(graph.nodes.Count);
+        Debug.Log(graph.edges.Count);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color nodeColor = Color.red;
+        Color edgeColor = Color.green;
+        float nodeSize = 0.1f;
+        // Draw nodes
+        Gizmos.color = nodeColor;
+        foreach (var node in graph.nodes.Values)
+        {
+            Gizmos.DrawSphere(node.Position, nodeSize);
+        }
+
+        // Draw edges
+        Gizmos.color = edgeColor;
+        foreach (var edge in graph.edges)
+        {
+            Gizmos.DrawLine(edge.Item1.Position, edge.Item2.Position);
+        }
+    }
+}
